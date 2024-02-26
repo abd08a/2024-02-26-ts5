3965df211705e1a9bab741fc794fcf7ed1cd30c7
abd08a <aabadini@gmail.com>
2024-02-26 11:03:23 +0200

    elements by id

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 console.log('veikia');
+var userName = document.getElementById('user-name');
+var userEmail = document.getElementById('user-email');
+var userPassword = document.getElementById('user-password');
+var mainButton = document.getElementById('main-btn');
--- index.ts
+++ index.ts
@@ -1,1 +1,16 @@
 console.log('veikia');
+
+type UserType = {
+  userName: string;
+  email: string;
+  password: string;
+}
+
+const userName: HTMLInputElement = document.getElementById('user-name') as HTMLInputElement 
+
+const userEmail: HTMLInputElement = document.getElementById('user-email') as HTMLInputElement 
+
+const userPassword: HTMLInputElement = document.getElementById('user-password') as HTMLInputElement 
+
+const mainButton: HTMLButtonElement = document.getElementById('main-btn') as HTMLButtonElement
+
